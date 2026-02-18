7a42ce6fe9b41ad70875877803a3d8120fe6139f
smaneet016   <msingh411@my.bcit.ca>
2026-02-17 22:57:52 -0800

    corrected images

--- src/components/site-footer.js
+++ src/components/site-footer.js
@@ -3,7 +3,7 @@ class SiteFooter extends HTMLElement {
         this.innerHTML = `
             <!-- Footer: single source of truth -->
             <footer class="py-3 my-4 border-top text-center bg-light">
-            <img src="/heart.gif" alt="Logo" width="40" class="mb-2">
+            <img src="images/heart.gif" alt="Logo" width="40" class="mb-2">
             <p class="mb-0 text-muted">&copy; 2026 Maneet Singh BCIT COMP1800</p>
             </footer>
         `;
