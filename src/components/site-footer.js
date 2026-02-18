fd3a46fa5ca7a528cdaf5cfe0c05316cd766da31
smaneet016   <msingh411@my.bcit.ca>
2026-02-17 22:56:53 -0800

    comit

--- src/components/site-footer.js
+++ src/components/site-footer.js
@@ -2,8 +2,9 @@ class SiteFooter extends HTMLElement {
     connectedCallback() {
         this.innerHTML = `
             <!-- Footer: single source of truth -->
-            <footer class="py-3 my-4 border-top text-center">
-                <p class="mb-0 text-muted">&copy; 2026 BCIT COMP1800</p>
+            <footer class="py-3 my-4 border-top text-center bg-light">
+            <img src="/heart.gif" alt="Logo" width="40" class="mb-2">
+            <p class="mb-0 text-muted">&copy; 2026 Maneet Singh BCIT COMP1800</p>
             </footer>
         `;
     }
